00adfb282621da53f85f7a3788c356299ce1fd0b
EduardooPV <eduardoveltroni@hotmail.com>
2022-05-20 17:00:57 -0300

    fix: corrige exportacao dos theme

--- lib/components.ts
+++ lib/components.ts
@@ -1,4 +1,4 @@
-export { Button } from '@lib/components/button';
+export { Button } from "@lib/components/button";
 
-export {colors } from '@lib/theme/theme'
-export { typograph } from '@lib/theme/theme'
+export { colors } from "@lib/theme/colors/colors";
+export { typograph } from "@lib/theme/typograph/typograph";
--- lib/theme/theme.ts
+++
@@ -1,8 +0,0 @@
-import Colors from "@lib/theme/colors/colors.json";
-import Typograph from "@lib/theme/typograph/typograph.json";
-
-export const colors = { ...Colors };
-export const typograph = { ...Typograph };
-
-export type ColorProps = typeof colors;
-export type TypographProps = typeof typograph;
